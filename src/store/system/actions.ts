08e742b07d25fcead6c137f09cb12d118e0327de
Vincent Ramdhanie <vramdhanie@gmail.com>
2019-11-30 15:19:59 -0400

    Add type for changeMode action

--- src/store/system/actions.ts
+++ src/store/system/actions.ts
@@ -1,8 +1,14 @@
-import { SystemState, PHASE_TRANSITION } from "./types";
+import { SystemState, PHASE_TRANSITION, CHANGE_MODE } from "./types";
 
 export function phaseTransition(newPhase: SystemState) {
   return {
     type: PHASE_TRANSITION,
     payload: newPhase
+  };
+}
+
+export function changeMode() {
+  return {
+    type: CHANGE_MODE
   };
 }
