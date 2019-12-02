7c15b9e6a1549c56c9a86f15d432fb46e2f379f5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-02 03:00:52 +0000

    Deploy vramdhanie/quiz to github.com/vramdhanie/quiz.git:gh-pages

---
+++ precache-manifest.ae9fcbc7c1f246df24e483ab503b5ff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4019675aac3307b201903977b8b38c",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "5eda48e8bdb519741149",
+    "url": "/quiz/static/css/main.62d9b4a0.chunk.css"
+  },
+  {
+    "revision": "2a8b093060c69f4fbe8a",
+    "url": "/quiz/static/js/2.321d7642.chunk.js"
+  },
+  {
+    "revision": "5eda48e8bdb519741149",
+    "url": "/quiz/static/js/main.c3bfa80f.chunk.js"
+  },
+  {
+    "revision": "cfd965e3f6f2d552ac4b",
+    "url": "/quiz/static/js/runtime-main.a4199980.js"
+  },
+  {
+    "revision": "49e22eaf239f40c78f0fc43bb9292c29",
+    "url": "/quiz/static/media/logo.49e22eaf.png"
+  }
+]);
